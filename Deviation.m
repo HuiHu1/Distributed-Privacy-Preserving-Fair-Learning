a5f2c0b63c373d6925aa72736621c3313946c0f6
HuiHu1 <43657104+HuiHu1@users.noreply.github.com>
2020-01-18 21:23:22 -0700

    Update Deviation.m

--- Deviation.m
+++ Deviation.m
@@ -1,17 +1,6 @@
-function [diviation_SP,diviation_DI,diviation_Error] = Deviation(SP,DI,data1,iter)
-Avg_SP=mean(SP);
-Avg_DI=mean(DI);
-Avg_Error=mean(data1);
-total_SP=0;
-total_DI=0;
-total_Error=0;
-for i=1:iter
-    total_SP = total_SP+(SP(i)-Avg_SP)^2;
-    total_DI = total_DI+(DI(i)-Avg_DI)^2;
-    total_Error =total_Error+(data1(i)-Avg_Error)^2;
-end
-diviation_SP = sqrt((1/iter)*total_SP);
-diviation_DI = sqrt((1/iter)*total_DI);
-diviation_Error = sqrt((1/iter)*total_Error);
+function [diviation_SP,diviation_DI,diviation_Error] = Deviation(SP,DI,data1)
+diviation_SP = std(SP);
+diviation_DI = std(DI);
+diviation_Error = std(data1);
 end
 
