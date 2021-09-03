ef3926e59a8003ce0363393281414f6444e3e98a
HuiHu1 <43657104+HuiHu1@users.noreply.github.com>
2021-09-02 20:31:40 -0600

    Update NewThirdParty.m

--- NewThirdParty.m
+++ NewThirdParty.m
@@ -3,9 +3,8 @@ trainset = randomset(1:12000,loop);
 train_Sensitive=DataSample(trainset(:,1),15);
 threshold=covariance; 
 [a,b]=size(W_old);
-s1 = train_Sensitive-mean(train_Sensitive)
-len = 12000
-cov_b = transpose(s1)*non_sensitive/len
+temp = cov(non_sensitive,train_Sensitive);
+cov_b = temp(1,2);
 if(abs(cov_b)<=threshold)
     W_fair = W_old;
 else
@@ -13,3 +12,6 @@ else
        W_fair(i,1)=0; 
     end
 end
+%s1 = train_Sensitive-mean(train_Sensitive)
+%len = 12000
+%cov_b = transpose(s1)*non_sensitive/len
