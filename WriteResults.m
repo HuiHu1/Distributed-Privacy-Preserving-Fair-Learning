2f31dd91070cb1e731e575eb3b8f6e86bd14f649
HuiHu1 <43657104+HuiHu1@users.noreply.github.com>
2021-09-03 16:33:37 -0600

    Delete WriteResults.m

--- WriteResults.m
+++
@@ -1,3 +0,0 @@
-writetable(table(SP,DI),'Fairness.txt','Delimiter','\t');
-writetable(table(data1),'Error.txt','Delimiter','\t');
-writetable(table(diviation_SP,diviation_DI,diviation_Error),'Deviation.txt','Delimiter','\t');
